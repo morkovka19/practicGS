e2ee56492d885e9941a58b9dc94e1c09a24bbf4f
Polina Shapovalova <shapovalova.p.d@gmail.com>
2023-10-25 22:35:34 +0300

    add GDS button

--- react-app/src/components/Button/themes/basic.ts
+++ react-app/src/components/Button/themes/basic.ts
@@ -1,7 +1,5 @@
-import { BaseThemeState, Button, EnumLike, OptionizedCSS, StyleDefinition, extractCSSOption, scale, typography, useThemeCSS } from "@greensight/gds";
-import { ButtonStateFull, State } from "../types";
-import { SVGRIcon } from "@greensight/gds/types/src/types/Utils";
-import { ReactNode, FC, Ref, useMemo, forwardRef } from "react";
+import { EnumLike, OptionizedCSS, StyleDefinition, extractCSSOption, scale, typography} from "@greensight/gds";
+import { ButtonStateFull} from "../types";
 import { colors } from "src/scripts/gds";
 import { Variant, Size } from "../enums";
 
@@ -10,79 +8,9 @@ interface ButtonTheme<V extends EnumLike, S extends EnumLike> {
     icon: StyleDefinition<ButtonStateFull<V, S>>;
 }
 
-interface ButtonProps<V extends EnumLike, S extends EnumLike>
-    extends Partial<BaseThemeState<V, S, ButtonTheme<V, S>>>, 
-        Partial<State> {
-    children?: ReactNode;
-    Icon?: SVGRIcon | FC<any>;
-    external?: boolean;
-    block: false, // theme, variant, size будут // будет типизированы дальше, поэтому не имеют значения по-умолчанию
-    iconAfter: boolean, // важно дать значения по-умолчанию для параметров состояния,
-    hidden?: boolean, // чтобы избежать null check внутри темы
-    type: string,
-    css: {}
-}
-
-const BaseButton = <V extends EnumLike, S extends EnumLike>(
-    {
-        children,
-        block = false,
-        theme, // theme, variant, size будут
-        size, // будет типизированы дальше, поэтому не имеют значения по-умолчанию
-        variant,
-        Icon,
-        iconAfter = false, // важно дать значения по-умолчанию для параметров состояния,
-        hidden = false, // чтобы избежать null check внутри темы
-        type = 'button',
-        external = false,
-        disabled = false,
-        rounded = true,
-        css
-    }: ButtonProps<V, S>,
-    ref: Ref<HTMLButtonElement>,
-) =>{
-    const state = useMemo<ButtonStateFull<V, S>>(
-        () => ({
-            disabled,
-            hidden,
-            size,
-            variant,
-            block,
-            iconAfter,
-            rounded,
-        }),
-        [block, disabled, hidden, iconAfter, size, variant, rounded],
-    );
-
-    if (!theme) {
-        throw new Error('[Button] theme is required');
-    }
-
-    const { button: totalCSS, icon: iconCSS } = useThemeCSS(theme!, state);
-    
-}
-
-
 export function createButtonWithTheme<T, U>(basic: ButtonTheme<typeof Variant, typeof Size>, primary: Variant, sm: Size) {
     throw new Error("Function not implemented.");
-}
-
-// export const createButtonWithTheme = <V extends EnumLike, S extends EnumLike>(
-//     defaultTheme: ButtonTheme<V, S>, // тема по-умолчанию
-//     defaultVariant: V | keyof V, // вариант по-умолчанию
-//     defaultSize: S | keyof S, // размер по-умолчанию
-// ) => {
-//     const ButtonRef = forwardRef(BaseButton) as typeof BaseButton;
-//     type ButtonReturn = ReturnType<typeof ButtonRef>;
-
-//     const ThemedButton = (({ theme = defaultTheme, variant = defaultVariant, size = defaultSize, ...props }, ref) => (
-//         <ButtonRef ref={ref} theme={theme} variant={variant} size={size} {...props} />
-//     )) as (props: ButtonProps<V, S>, ref: Ref<HTMLButtonElement>) => ButtonReturn;
-
-//     (ThemedButton as any).displayName = 'Button';
-
-//     return forwardRef(ThemedButton) as typeof ThemedButton;
-// };
+};
 
 const basicTheme: ButtonTheme<typeof Variant, typeof Size> = {
     button: state => {
@@ -101,6 +29,8 @@ const basicTheme: ButtonTheme<typeof Variant, typeof Size> = {
             "primary": {
                 backgroundColor: colors.blue,
                 color: colors.white,
+                borderRadius: scale(1, true),
+                size: 44,
                 ':hover': {
                     backgroundColor: colors.blueHover,
                 },
@@ -111,14 +41,15 @@ const basicTheme: ButtonTheme<typeof Variant, typeof Size> = {
             },
             "secondary": {
                 backgroundColor: colors.grey900,
+                color: colors.white,
+                borderRadius: scale(1, true),
                 ':hover': {
                     backgroundColor: colors.black
                 }
-            },
+            }
         };
 
         return {
-            /// common styles...
             ...extractCSSOption(size, state.size),
             ...extractCSSOption(variant, state.variant),
         };
@@ -128,11 +59,11 @@ const basicTheme: ButtonTheme<typeof Variant, typeof Size> = {
             sm: {
                 width:"16px",
                 height: "16px",
-                // ...(typography('buttonSm') as any),
+                ...(typography('buttonSm') as any),
             },
             md: {
                 padding:  `${scale(1, true) + 0.5}px ${scale(1)}px`,
-                // ...(typography('buttonMd') as any)
+                ...(typography('buttonMd') as any)
             },
         };
         return{
--- react-app/src/pages/index.tsx
+++ react-app/src/pages/index.tsx
@@ -15,21 +15,15 @@ export default function HomePage(props: any) {
 
   const vacancies = useAllVacancies(props);
   return (
-  //   <Body>
-  //     {/* <Head children={undefined}></Head> */}
+
     <div>
-      {/* {JSON.stringify(vacancies.items)} */}
-      
-<Button variant="secondary" size="md" rounded={true} css={{ background: "red" }} block={false} iconAfter={false} hidden={false} type={'button'} disabled={false} getTypographyCSS={function (name: 'body' | 'buttonLg' | 'buttonMd' | 'buttonSm' | 'captionUppercase' | 'caption' | 'smallBold' | 'small' | 'bodyBold' | 'subheading' | 'title' | 'headline' | 'h4' | 'h3' | 'h2' | 'h1'): CSSObject {
+      {JSON.stringify(vacancies.items)}
+
+      <Button variant="primary" size="sm" rounded type={'button'} getTypographyCSS={function (name: 'body' | 'buttonLg' | 'buttonMd' | 'buttonSm' | 'captionUppercase' | 'caption' | 'smallBold' | 'small' | 'bodyBold' | 'subheading' | 'title' | 'headline' | 'h4' | 'h3' | 'h2' | 'h1'): CSSObject {
         throw new Error('Function not implemented.');
-      } }>gfhfgdh</Button>
-      </div>
+      }}>gfhfgdh</Button>
+    </div>
 
-  //     <Header />
-  //     <Main />
-  //     <Footer />
-
-  //   <Body/>
 
   );
 };
